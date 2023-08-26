e1e1c4b96c57f1cb223df3fab3feb98666455df2
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-26 18:22:29 -0300

    tests: create product unit tests

---
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using CleanArchmvc.Domain.Entities;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact(DisplayName = "Create Product With Valid State")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "Product Image");
+            action.Should()
+                .NotThrow<CleanArchmvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Can't Create Product With Negative Id")]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "Product Image");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+        [Fact(DisplayName = "Can't Create Product With Short Name")]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "Product Image");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation> ()
+                .WithMessage("Invalid name. To shortm minimun 3 characters");
+        }
+
+        [Fact(DisplayName = "Can't Create Product With Null Name")]
+        public void CreateProduct_NullName_DomainExceptionRequiresDescription()
+        {
+            Action action = () => new Product(1, null, "Product Description", 9.99m, 99, "Product Image");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact(DisplayName = "Can't Create Product With Short Description")]
+        public void CreateProduct_ShortDescription_DomainExceptionShortDescriprion()
+        {
+            Action action = () => new Product(1, "Product Name", "Pr", 9.99m, 99, "Product Image");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid description. To shortm minimun 3 characters");
+        }
+
+        [Fact(DisplayName = "Can't Create Product With Null Description")]
+        public void CreateProduct_NullDescription_DomainExceptionRequiredDescription()
+        {
+            Action action = () => new Product(1, "Product Name", null, 9.99m, 99, "Product Image");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid description. Name is required");
+        }
+
+        [Fact(DisplayName = "Can't Create Product With Long Image Name")]
+        public void CreateProduct_LongImageName_DomainExceptionLongImageName()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat nec justo in fermentum. Fusce euismod leo nec justo vehicula, vel feugiat ipsum hendrerit. Integer hendrerit tristique lacinia. Nullam vel augue ac nulla facilisis ultrices. Sed vel erat vitae sapien consequat malesuada.");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid image name, too long, maximum 250 characters");
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Name")]
+        public void CreateProduct_WithNullImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<CleanArchmvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Name Don't Throw NullReferenceException")]
+        public void CreateProduct_WithNullImageName_NoNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<NullReferenceException>();
+        }
+
+        [Fact(DisplayName = "Create Product With Empty Image Name")]
+        public void CreateProduct_WithEmptyImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
+            action.Should()
+                .NotThrow<CleanArchmvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Negative Price")]
+        public void CreateProduct_InvalidPriceValue_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -9.99m, 99, "Product Image");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation> ()
+                .WithMessage("Invalid price value");
+        }
+
+        [Theory(DisplayName = "Create Product With Negative Stock Value")]
+        [InlineData(-5)]
+        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value)
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, value, "Product Image");
+            action.Should()
+                .Throw<CleanArchmvc.Domain.Validation.DomainExceptionValidation> ()
+                .WithMessage("Invalid stock value");
+        }
+    }
+}
