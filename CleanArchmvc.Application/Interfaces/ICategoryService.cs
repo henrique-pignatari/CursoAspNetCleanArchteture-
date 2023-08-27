8fa204056c11581011986c45bfdefcdd0a00069e
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-26 23:47:06 -0300

    feat: create services interfaces for application

---
+++ CleanArchmvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchmvc.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchmvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategoriesAsync();
+        Task<CategoryDTO> GetByIdAsync(int? id);
+        Task AddAsync(CategoryDTO categoryDTO);
+        Task UpdateAsync(CategoryDTO categoryDTO);
+        Task RemoveAsync(int? id);
+
+    }
+}
---
+++ CleanArchmvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchmvc.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchmvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProductsAsync();
+        Task<ProductDTO> GetByIdAsync(int? id);
+        Task<ProductDTO> GetProductCategoryAsync(int? id);
+        Task AddAsync(ProductDTO productDTO);
+        Task UpdateAsync(ProductDTO productDTO);
+        Task RemoveAsync(int? id);
+    }
+}
