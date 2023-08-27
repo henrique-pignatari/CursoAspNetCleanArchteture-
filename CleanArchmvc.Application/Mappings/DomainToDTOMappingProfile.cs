003d503f48234712632c984a65270628be0054c4
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-26 23:12:38 -0300

    feat: add entities DTOs and configure mappings

---
+++ CleanArchmvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CleanArchmvc.Application.DTOs;
+using CleanArchmvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchmvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
