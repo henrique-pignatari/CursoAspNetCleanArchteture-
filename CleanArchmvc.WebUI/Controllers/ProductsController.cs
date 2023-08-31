26f8b41a3e9d7318bd0b7cf110100d880653bf79
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-30 22:49:40 -0300

    feat: add product create page

--- CleanArchmvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchmvc.WebUI/Controllers/ProductsController.cs
@@ -1,14 +1,18 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -16,6 +20,25 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             var products = await _productService.GetProductsAsync();
             return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _categoryService.GetCategoriesAsync();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDTO)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productService.AddAsync(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDTO);
         }
     }
 }
