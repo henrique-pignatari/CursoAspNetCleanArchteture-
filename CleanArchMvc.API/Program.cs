15e4d9e490a5a89fa73b56cf19148edaf4c9cabd
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-08 00:11:29 -0300

    feat: move swagger dependecy injectio to IoC project, add JWT swagger configuration

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -1,4 +1,5 @@
 using CleanArchMvc.Infra.IoC;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureSwagger();
 builder.Services.AddInfrastructureAPI(builder.Configuration);
 builder.Services.AddInfrastructureJWT(builder.Configuration);
 
