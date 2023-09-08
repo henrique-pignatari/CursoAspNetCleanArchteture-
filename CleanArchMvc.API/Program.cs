631c3faf0d7d12a7f6d6c1f9c093a4c8bdf923db
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-07 23:56:03 -0300

    feat: add jwt configuration dependency injection

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureAPI(builder.Configuration);
+builder.Services.AddInfrastructureJWT(builder.Configuration);
 
 
 var app = builder.Build();
